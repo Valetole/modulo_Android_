965c403526dc362c579bc9985b28eef64192243b
valetole <valentina.munoz.canas@gmail.com>
2021-07-26 03:14:52 -0400

    Se crea la activity principal, se añaden los botones de "iniciar sesion", "crear registro" e "ingresar como invitado/a"

--- app/src/main/java/cl/valentina/miapp/MainActivity.java
+++ app/src/main/java/cl/valentina/miapp/MainActivity.java
@@ -2,7 +2,10 @@ package cl.valentina.miapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button botonInicioSesion = findViewById(R.id.boton_inicioSesion);
+        botonInicioSesion.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), PantallaInicioSesion.class);
+                startActivity(intent);
+            }
+        });
+        Button botonCrearRegistro = findViewById(R.id.boton_CrearRegistro);
+        botonCrearRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), PantallaCrearRegistro.class);
+                startActivity(intent);
+            }
+        });
+
+
 
     }
 }
---
+++ app/src/main/java/cl/valentina/miapp/PantallaCrearRegistro.java
@@ -0,0 +1,14 @@
+package cl.valentina.miapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PantallaCrearRegistro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_crear_registro);
+    }
+}
