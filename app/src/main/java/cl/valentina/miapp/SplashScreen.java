e4b577a5da43c73b2d2d6155cffecea9ea97a350
valetole <valentina.munoz.canas@gmail.com>
2021-07-26 00:57:20 -0400

    Se sube icono de la app y se crea splash screen de inicio

---
+++ app/src/main/java/cl/valentina/miapp/SplashScreen.java
@@ -0,0 +1,43 @@
+package cl.valentina.miapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        Animation animacionSplashScreen1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation animacionSplashScreen2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+        TextView titulo_app= findViewById(R.id.tituloAppTextView);
+        ImageView logo_app= findViewById(R.id.logoImageView);
+
+        titulo_app.setAnimation(animacionSplashScreen2);
+        logo_app.setAnimation(animacionSplashScreen1);
+
+        new Handler().postDelayed (new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
+    }
+}
