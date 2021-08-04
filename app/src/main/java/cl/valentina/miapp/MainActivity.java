3b9d162246c4bf48b4ad4a047e31bf26d32e74f1
valetole <valentina.munoz.canas@gmail.com>
2021-08-04 02:46:24 -0400

    Se crea menu principal de app con navigation drawer

--- app/src/main/java/cl/valentina/miapp/MainActivity.java
+++ app/src/main/java/cl/valentina/miapp/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import cl.valentina.miapp.view.PantallaInicioSesion;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
